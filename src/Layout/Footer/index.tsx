d087f58e63fbae1132b30be2fe9850831a3e33cc
Manuel Bieh <git@manuelbieh.de>
2024-02-06 00:23:24 +0100

    chore: allow adding className prop to footer

--- src/Layout/Footer/index.tsx
+++ src/Layout/Footer/index.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
+import classNames from 'classnames';
 import css from './Footer.module.css';
 
-type PropsT = {
+type Props = {
     children: any;
+    className?: string;
 };
 
-export const Footer = ({ children }: PropsT) => <footer className={css.wrapper}>{children}</footer>;
+export const Footer: React.FC<Props> = ({ children, className }: Props) => (
+    <footer className={classNames(css.wrapper, className)}>{children}</footer>
+);
