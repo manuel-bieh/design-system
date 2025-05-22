edc46f805a448d91eebd94aa1f73f549d1450ff1
Manuel Bieh <git@manuelbieh.de>
2025-05-22 13:05:12 +0200

    chore: cleanup

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # ManuelBieh.de Design System
 
 Tiny design system for my own website, based on the great [TimMikeladze/tsup-react-package-starter](https://github.com/TimMikeladze/tsup-react-package-starter) starter, adjusted to fit my personal needs.
-
-<!-- test -->
--- vitest.config.js
+++ vitest.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    setupFiles: './tests/setup.js',
-    passWithNoTests: true,
-  },
+    plugins: [react()],
+    test: {
+        environment: 'jsdom',
+        setupFiles: './tests/setup.js',
+        passWithNoTests: true,
+    },
 });
